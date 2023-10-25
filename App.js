3f1b54ca288f62f4a16780db6015ca416028ee49
Ahsan Farooq <ahsanfarooqxx69@gmail.com>
2023-10-25 12:42:07 +0500

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// In App.js in a new project
 
-export default function App() {
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import AppNavigator from './src/navigations/AppNavigator';
+
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <AppNavigator/>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,42 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React, {useLayoutEffect} from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { Ionicons } from '@expo/vector-icons';  
+import Header from '../components/Header';
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: true,
+      title: 'Booking.com',
+      headerTitleStyle: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'white'
+      },
+      headerStyle: {
+        backgroundColor: '#003580',
+        height: 110,
+      },
+      headerRight: () => (
+        <Ionicons name="notifications-outline" size={24} color="white" style={{marginRight: 12}} />
+      )
+    })
+  }, [])
+
+  return (
+    <View>
+      <Header/>
+    </View>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+  a:{
+    
+  }
+})
